0e6e0e8f0680031db1f9ae6a7bfbd72937b2dc31
ana <ana.maria.guitart@upc.edu>
2021-10-06 15:15:09 +0200

    enumera departments /departments

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Department;
+use App\Repository\DepartmentRepository;
+use App\Repository\PersonaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class DepartmentController extends AbstractController
 {
     /**
-     * @Route("/department", name="department")
+     * @Route("/prova", name="prova")
      */
-    public function index(): Response
+    public function index_prova(Environment $twig, DepartmentRepository $departmentRepository): Response
     {
         return new Response(<<<EOF
-<html>
- <body>
- <h1>hola guapi!!!!</h1>
-</body>
-</html>
+                            <html>
+                            <body>fuckyou!!!</body>
+                            </html>
 EOF
- );
+                            );
+    }
+
+    /**
+     * @Route("/departments", name="departments")
+     */
+    public function index(Environment $twig, DepartmentRepository $departmentRepository): Response
+    {
+        return new Response($twig->render('department/index.html.twig',['departments'=> $departmentRepository->findAll()]));
+    }
+
+    /**
+     * @Route("/department/{id}", name="department_id")
+     */
+    public function show(Environment $twig, Department $department, PersonaRepository $personaRepository ): Response
+    {
+        return new Response($twig->render('department/show.html.twig',['department'=> $department, 'persones' => $personaRepository->findby(['department'=> $department],['cognoms'=> 'ASC'])]));
     }
 }
