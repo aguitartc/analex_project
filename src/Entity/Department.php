d2549a80a52efab6979134f29d3ba1a17a7e11f4
ana <ana.maria.guitart@upc.edu>
2021-10-07 13:09:28 +0200

    generando slugs, s'afegeix la func computeSlug

--- src/Entity/Department.php
+++ src/Entity/Department.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\DBAL\Schema\Constraint;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=DepartmentRepository::class)
@@ -151,5 +152,13 @@ class Department
         $this->slug = $slug;
 
         return $this;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug)
+        {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
     }
 }
